d54082f0964a38f2e02c770a4ab57d845bf07cba
xinmm <xinmm@bsfit.com.cn>
2018-11-12 19:19:54 +0800

    ajax 异步发送邮件已经实现，以后注册用户需发送邮件方可

--- src/main/java/com/mmxin/controller/IdentifiedController.java
+++ src/main/java/com/mmxin/controller/IdentifiedController.java
@@ -1,9 +1,12 @@
 package com.mmxin.controller;
 
+import com.mmxin.configure.ReturnCodeEnum;
 import com.mmxin.service.MailSenderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import javax.websocket.server.PathParam;
 
 
 @Controller
@@ -13,9 +16,16 @@ public class IdentifiedController {
     @Autowired
     MailSenderService mailSenderService;
 
-    @PostMapping()
+  /*  @PostMapping()
     public @ResponseBody String send(@RequestParam("email") String email){
         return mailSenderService.send(email,null);
+    }*/
+
+    @GetMapping("/{email}")
+    public @ResponseBody String sendGet(@PathVariable("email") String email){
+
+        return ReturnCodeEnum.MailSenderSuccess;
+        //return mailSenderService.send(email,null);
     }
 
 }
